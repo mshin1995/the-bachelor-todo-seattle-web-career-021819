c2b52465cd46990d9894cdf22946e65d5d089f3e
Matthew Shin <mshin1995@gmail.com>
2019-02-20 02:53:00 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  data[occupation].each do |info|
+  data.each do |season, info|
     if info[""]
 end
 
