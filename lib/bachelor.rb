b126762c2fe24f67a168217ad11ccb3c8d5c9b8a
Matthew Shin <mshin1995@gmail.com>
2019-02-20 03:12:02 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |info|
+    if info["status"] = "Winner"
+      return info["name"].split(" ").first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |info|
+      if info["occupation"] == occupation
+        return info["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |info|
+      if info["hometown"] == hometown
+        counter += 1 
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |info|
+      if info["hometown"] == hometown
+        return info["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = 0 
+  contestants = 0 
+  data[season].each do |info|
+    total_age += info["age"].to_i
+    contestants += 1 
+  end
+  (total_age / contestants.to_f).round(0)
 end
