6de516cd222ca0696bfd4fcb0c9fe444a2fa90b4
Matthew Shin <mshin1995@gmail.com>
2019-02-20 03:04:41 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -32,7 +32,7 @@ def get_occupation(data, hometown)
   data.each do |season, contestants|
     contestants.each do |info|
       if info["hometown"] == hometown
-        
+        return 
 end
 
 def get_average_age_for_season(data, season)
