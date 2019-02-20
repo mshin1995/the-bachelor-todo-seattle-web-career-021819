2547b16f358cdd6a2d8af7f7ae53af97fc0a61e5
Matthew Shin <mshin1995@gmail.com>
2019-02-20 02:49:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,5 @@
 def get_first_name_of_season_winner(data, season)
-  data[season].each do | info|
+  data[season].each do |info|
     if info[:status] = "Winner"
       return info[:name].split(" ").first
     end
